2cdd73c81f888157bd764e96a9a970ad3c7c4a16
Allan.W.C <allan_195@hotmail.com>
2017-09-21 11:13:20 +1000

    Bookmarks API identified in postman
     - First stage of hooking up API complete

---
+++ api/controllers/bookmarkController.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+  Bookmark = mongoose.model('Bookmark');
+
+exports.list_all_bookmarks = function(req, res) {
+  Bookmark.find({}, function(err, bookmark) {
+    if (err)
+      res.send(err);
+    res.json(bookmark);
+  });
+};
+
+exports.create_a_bookmark = function(req, res) {
+  var new_bookmark = new Bookmark(req.body);
+  new_bookmark.save(function(err, bookmark  ) {
+    if (err)
+      res.send(err);
+    res.json(bookmark);
+  });
+};
+
+
+exports.read_a_bookmark = function(req, res) {
+  Bookmark.findById(req.params.bookmarkId, function(err, bookmark) {
+    if (err)
+      res.send(err);
+    res.json(bookmark);
+  });
+};
+
+
+exports.update_a_bookmark = function(req, res) {
+  Bookmark.findOneAndUpdate({_id: req.params.bookmarkId}, req.body, {new: true}, function(err, bookmark) {
+    if (err)
+      res.send(err);
+    res.json(bookmark);
+  });
+};
+
+
+exports.delete_a_bookmark = function(req, res) {
+
+  Bookmark.remove({
+    _id: req.params.bookmarkId
+  }, function(err, bookmark) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Bookmark successfully deleted' });
+  });
+};
---
+++ api/routes/bookmarkRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  const bookmark = require('../controllers/bookmarkController');
+
+  // todoList Routes
+  app.route('/bookmarks')
+    .get(bookmark.list_all_bookmarks)
+    .post(bookmark.create_a_bookmark);
+
+
+  app.route('/bookmarks/:bookmarkId')
+    .get(bookmark.read_a_bookmark)
+    .put(bookmark.update_a_bookmark)
+    .delete(bookmark.delete_a_bookmark);
+};
